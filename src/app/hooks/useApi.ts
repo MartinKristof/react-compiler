924ce7efc3f1654ea0d02740cf913cde88e3346f
Martin Krištof <martin.kristof@heureka.group>
2024-10-24 09:49:38 +0200

    fix: useCallback in hooks

--- src/app/hooks/useApi.ts
+++ src/app/hooks/useApi.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 type TPost = {
   userId: number;
@@ -13,14 +13,14 @@ export const useApi = () => {
   const [data, setData] = useState<TPost[]>([]);
   const [loading, setLoading] = useState(false);
 
-  const getData = async (searchParam: string) => {
+  const getData = useCallback(async (searchParam: string) => {
     setLoading(true);
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${searchParam}`);
     const json = (await response.json()) as TPost[];
 
     setData(json);
     setLoading(false);
-  };
+  }, []);
 
   return { loading, data, getData, setData };
 };
